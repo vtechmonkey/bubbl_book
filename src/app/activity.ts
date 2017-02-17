7bc419a33975f9014fc9e5f95ecfb4b2d597539b
ainehehir <ainehehir@gmail.com>
2017-02-17 16:46:43 +0000

    activity model

--- src/app/activity.ts
+++ src/app/activity.ts
@@ -1,15 +1,5 @@
 export class Activity {
-<<<<<<< HEAD
-	id: number;
-	name: string;
-	venue: string; 
-	price: string;
-	date: string;
-	time: string;
-	category: string;
-	imageURL: string;
-}
-=======
+
 	_id:String;
 	name: String;
 	venue: String;
@@ -20,4 +10,4 @@ export class Activity {
 	
 }
 
->>>>>>> 87a2a08a560cda03a3bf9682017c982ead129b36
+
