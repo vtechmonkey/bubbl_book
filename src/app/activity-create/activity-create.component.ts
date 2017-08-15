80d8509d85bd99193d6b1824e34836a57b21e31c
Sarah <sarah.oconnor@webinnovate.ie>
2017-08-15 19:07:01 +0100

    change local storage to handle empty values

--- src/app/activity-create/activity-create.component.ts
+++ src/app/activity-create/activity-create.component.ts
@@ -32,7 +32,7 @@ import { ISubCategory } from '../subCategory/subCategory';
 })
 
 
-export class ActivityCreateComponent implements OnInit, OnChanges {
+export class ActivityCreateComponent implements OnInit {
 
   title:string = "Create your own Event";
   location:Location;
@@ -86,10 +86,10 @@ localStorageForm:any;
       this.location = location;
       this.url = this.location.path();         
       this.pics.imageURL = pics.imageURL; // default string or image url of image uploaded with pics service
-      // this.localStorageForm = (JSON.parse(localStorage.getItem('activity')));
-      // console.log(this.localStorageForm);
-      // console.log(this.localStorageForm.description);
-     
+      this.localStorageForm = (JSON.parse(localStorage.getItem('activity')));
+      console.log(this.localStorageForm);
+     // console.log(this.localStorageForm.description);
+  
      
 } //constructor function 
  
@@ -100,58 +100,67 @@ localStorageForm:any;
       )
 
         console.log(this.auth.userProfile);
-        //  console.log(this.auth.userProfile.email);
+        console.log(this.auth.userProfile.email);
         if (this.auth.userProfile){
           console.log(this.auth.userProfile.user_id);
           }
 
-     //     if(this.localStorageForm){
-     //          this.activityForm  
-     //           .patchValue({name:this.localStorageForm.name})
-     //          this.activityForm
-     //           .patchValue({venue:this.localStorageForm.venue})
-     //          this.activityForm
-     //           .patchValue({description:this.localStorageForm.description})
-     //          this.activityForm
-     //           .patchValue({category:this.localStorageForm.category})              
-     //          this.activityForm  
-     //           .patchValue({subCategory:this.localStorageForm.subCategory})           
-     //          this.activityForm  
-     //           .patchValue({min:this.localStorageForm.min})            
-     //          this.activityForm  
-     //           .patchValue({max:this.localStorageForm.max})
-     //          this.activityForm  
-     //           .patchValue({publicActivity:this.localStorageForm.publicActivity})            
-     //          this.activityForm  
-     //           .patchValue({prices:this.localStorageForm.prices})      
-     //          this.activityForm  
-     //           .patchValue({dates:this.localStorageForm.dates})
-     //          this.activityForm  
-     //           .patchValue({imageURL:this.localStorageForm.imageURL})   
-     //      } 
+        if (!this.auth.userProfile){
+          console.log("ain't nobody home girlfriend");
+          localStorage.removeItem('activity');
+          this.localStorageForm = undefined;
+
+        }
+
+         if(this.localStorageForm){
+              this.activityForm  
+               .patchValue({name:this.localStorageForm.name})
+              this.activityForm
+               .patchValue({venue:this.localStorageForm.venue})
+              this.activityForm
+               .patchValue({description:this.localStorageForm.description})
+              this.activityForm
+               .patchValue({category:this.localStorageForm.category})              
+              this.activityForm  
+               .patchValue({subCategory:this.localStorageForm.subCategory})           
+              this.activityForm  
+               .patchValue({min:this.localStorageForm.min})            
+              this.activityForm  
+               .patchValue({max:this.localStorageForm.max})
+              this.activityForm  
+               .patchValue({publicActivity:this.localStorageForm.publicActivity})            
+              this.activityForm  
+               .patchValue({prices:this.localStorageForm.prices})      
+              this.activityForm  
+               .patchValue({dates:this.localStorageForm.dates})
+              this.activityForm  
+               .patchValue({imageURL:this.localStorageForm.imageURL})   
+          } 
      }
 
-      ngOnChanges(changes: SimpleChanges) {
+     // onchanges won't detect a change to activity.value, just activity
+     //  ngOnChanges(changes: SimpleChanges) {
      //   console.log('is this thing on?');
      //   console.log(changes.activity);
      //   if(changes.activity){
      //    localStorage.setItem('activity', JSON.stringify(this.activityForm.value));
      //    console.log('from ng onchanges', this.activityForm.value);   
      //    }  
-     }
-     // ngDoCheck(){
-     //   if(this.activityForm.value != this.localStorageForm.value){
-     //      localStorage.setItem('activity', JSON.stringify(this.activityForm.value));
-     //     console.log('seems to be changes afoot');
-     //   }
      // }
+     ngDoCheck(){
+
+       if(this.activityForm.value){
+          localStorage.setItem('activity', JSON.stringify(this.activityForm.value));
+         console.log('seems to be changes afoot');
+       }
+     }
      
-     // localStore() {
-     //    localStorage.setItem('activity', JSON.stringify(this.activityForm.value));
-     //    console.log(this.activityForm.value);
+     localStore() {
+        localStorage.setItem('activity', JSON.stringify(this.activityForm.value));
+        console.log(this.activityForm.value);
          
-     //      console.log(this.localStorageForm.name);
-     //    }
+          console.log(this.localStorageForm.name);
+        }
       
                
        
